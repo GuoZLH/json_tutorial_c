3f98397231fad0c6208a4fbfe8a6c09e98de43af
guoz <843014747@qq.com>
2020-06-06 21:20:12 +0800

    second commit

--- leptjson.h
+++ leptjson.h
@@ -6,6 +6,7 @@ typedef enum{//枚举JSON的类型
 } lept_type;
 
 typedef struct{//JSON数据结构
+    double n;
     lept_type type;
 }lept_value;
 
@@ -13,11 +14,14 @@ enum{  //返回值
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,//空白
     LEPT_PARSE_INVALID_VALUE,//无效字符
-    LEPT_PARSE_ROOT_NOT_SINGULAR//值/空白后还有字符
+    LEPT_PARSE_ROOT_NOT_SINGULAR,//值/空白后还有字符
+    LEPT_PARSE_NUMBER_TOO_BIG
 };
 
 int lept_parse(lept_value* v, const char* json);//解析JSON
 
 lept_type lept_get_type(const lept_value* v);//获取类型
+
+double lept_get_number(const lept_value* v);//获取数字
 
 #endif /*LEPTJSON_H__*/
